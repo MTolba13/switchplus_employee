de395aa360cf411e89b2835e83f3cd375aef2a0f
Mahmoud Tolba <89683181+MTolba13@users.noreply.github.com>
2022-03-29 13:52:27 +0200

    On Boarding Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:switchplus_employee/presentation/screens/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnBoardingScreen(),
+    );
   }
 }
