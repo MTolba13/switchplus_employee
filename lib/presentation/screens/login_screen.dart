24c7ff6c09f061071737eaacc6a5379fad4ca715
Mahmoud Tolba <89683181+MTolba13@users.noreply.github.com>
2022-03-30 22:34:56 +0200

    Update login_screen.dart

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -5,35 +5,37 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Image.network(
-            'https://contact.eg/wp-content/uploads/2020/10/Switch-plus.png',
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            'Login',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Image.network(
+              'https://contact.eg/wp-content/uploads/2020/10/Switch-plus.png',
+              fit: BoxFit.cover,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            'Welcome',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+            const SizedBox(
+              height: 30,
             ),
-          ),
-        ],
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
